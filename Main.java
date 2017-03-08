9e561978e489a1f6393350613ba2056210629b48
Samuel C. Gurtler <StudentcareerSamgurtler@gmail.com>
2017-03-08 10:40:04 -0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -2,15 +2,16 @@ package EndUser;
 import javax.swing.JFrame;
 import java.awt.FlowLayout;
 import java.util.ArrayList;
+import java.awt.GridLayout;
 	public class Main{
 		public static void main(String[] args){
 			JFrame JFrame = new JFrame();
 			ArrayList<ArrayList<Question_Panel>> Arrlst_Questions = new ArrayList<ArrayList<Question_Panel>>();
-			
 			JFrame.setDefaultLookAndFeelDecorated(true);
-			//JFrame.setLayout();
+			JFrame.setLayout(new GridLayout(0,1));
 			JFrame.add(new Question_Panel("Is this a question?"));
 			JFrame.add(new Question_Panel("Is this a second question?"));
+			JFrame.setSize(new java.awt.Dimension(200,400));
 			JFrame.setVisible(true);
 			JFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); 
 		}
--- Question_Panel.java
+++ Question_Panel.java
@@ -3,23 +3,37 @@ import javax.swing.JLabel;
 import javax.swing.ButtonGroup;
 import javax.swing.JRadioButton;
 import javax.swing.JPanel;
+import java.awt.GridLayout;
+import javax.swing.SpringLayout;
+import javax.swing.GroupLayout;
+import java.awt.FlowLayout;
 public class Question_Panel extends JPanel{
 			private JLabel Question;
 			private ButtonGroup CBG = new ButtonGroup();
 			protected Question_Panel(String Question){
 				this.Question = new JLabel(Question);
-				add(this.Question);
 				JRadioButton Safe = new JRadioButton("Safe",false);
 				JRadioButton Unsafe = new JRadioButton("Unsafe",false);
 				JRadioButton Unobserved = new JRadioButton("Unobserved",false);
 				CBG.add(Safe);
 				CBG.add(Unsafe);
 				CBG.add(Unobserved);
-				add(Safe);
-				add(Unsafe);
-				add(Unobserved);
+				FlowLayout FlwLyt = new FlowLayout();
+				JPanel Question_Panel = new JPanel(FlwLyt);
+				Question_Panel.add(Safe);
+				Question_Panel.add(Unsafe);
+				Question_Panel.add(Unobserved);
 				setVisible(true);
-				setLayout(new java.awt.FlowLayout());
+				/*SpringLayout SprngLyt = new SpringLayout();
+				SprngLyt.addLayoutComponent("Question",this.Question);
+				SprngLyt.addLayoutComponent(Question_Panel,FlwLyt);*/
+				GroupLayout GrpLyt = new GroupLayout(this);
+				GrpLyt.addLayoutComponent("Question",this.Question);
+				GrpLyt.addLayoutComponent(Question_Panel,FlwLyt);
+				GrpLyt.setAutoCreateContainerGaps(true);
+				add(this.Question);
+				add(Question_Panel);
+				//setLayout(SprngLyt);
 			}
 			protected String getAnswer(){
 				return ((JRadioButton) CBG.getSelection()).getText();
