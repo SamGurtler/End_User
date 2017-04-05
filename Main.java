b198539cf9e632f539fcf27e14978ea3a99e6d66
Samuel C. Gurtler <StudentcareerSamgurtler@gmail.com>
2017-04-05 14:04:42 -0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -7,41 +7,42 @@ import java.awt.GridLayout;
 import java.util.Scanner;
 import java.io.File;
 import javax.swing.JLabel;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 	public class Main{
 		public static void main(String[] args)throws java.io.FileNotFoundException{
 			ArrayList<JFrame> JFrame = new ArrayList<JFrame>();
 			ArrayList<ArrayList<Question_Panel>> Arrlst_Questions = new ArrayList<ArrayList<Question_Panel>>();
 			ArrayList<JLabel> Arrlst_Sections = new ArrayList<JLabel>();
 			Scanner scan = new Scanner(new File("./Questions.txt"));
+			Pattern p = Pattern.compile("[^:]+:");
+			String line = "";
 			for(int counter = -1;scan.hasNext();counter+=0){
-				if(scan.hasNext(".*:?")){
+				line = scan.nextLine();
+				Matcher m = p.matcher(line);
+				if(m.matches()){
 					counter++;
-					scan.useDelimiter(":");
-					Arrlst_Sections.add(new JLabel(scan.nextLine()));
+					System.out.println("Making new JLabel titled:"+line);
+					Arrlst_Sections.add(new JLabel(line));
 					Arrlst_Questions.add(new ArrayList<Question_Panel>());
-					JFrame.add(new JFrame());
-					//JFrame.get(counter).setDefaultLookAndFeelDecorated(true);
-					//JFrame.get(counter).setLayout(new GridLayout(0,1));
+					System.out.println("Label ->"+line);
 				}
-				scan.useDelimiter(".*/?");
-				Arrlst_Questions.get(counter).add(new Question_Panel(scan.nextLine()));
+				else Arrlst_Questions.get(counter).add(new Question_Panel(line)); 
 			}
 			for(int count = 0;Arrlst_Sections.size()>count;count++){
 				Arrlst_Sections.get(count).setHorizontalAlignment((int)Arrlst_Sections.get(count).CENTER_ALIGNMENT);
+				JFrame.add(new JFrame());
+				System.out.println("What JLabel may actually say:"+Arrlst_Sections.get(count).getText());
 				JFrame.get(count).add(Arrlst_Sections.get(count));
-				for(int counter = 0;Arrlst_Questions.get(count).size()>counter;counter++){
-					JFrame.get(counter).add(Arrlst_Questions.get(count).get(counter));
+				for(int count1 = 0;Arrlst_Questions.get(count).size()>count1;count1++){
+					JFrame.get(count).add(Arrlst_Questions.get(count).get(count1));
+					System.out.println("Jframe.get("+count+").add(Arrlst_Questions.get("+count+").get("+count1+"));");
 				}
-				JFrame.get(count).pack();
-			}
-			for(int count = 0;Arrlst_Sections.size()>count;count++){
-				for(int counter = 0;Arrlst_Questions.get(count).size()>counter;counter++){
-					Arrlst_Questions.get(count).get(counter).addGap();
-				}
-			}
-			for(int count = 0;count<JFrame.size();count++){
-				JFrame.get(count).setVisible(true);
+				JFrame.get(count).setDefaultLookAndFeelDecorated(true);
 				JFrame.get(count).setDefaultCloseOperation(JFrame.get(count).DISPOSE_ON_CLOSE);
+				JFrame.get(count).setLayout(new GridLayout(0,1));
+				JFrame.get(count).pack();
+				JFrame.get(count).setVisible(true);
 			}
 		}
 	}
--- Question_Panel.java
+++ Question_Panel.java
@@ -7,15 +7,13 @@ import java.awt.GridLayout;
 import javax.swing.SpringLayout;
 import javax.swing.GroupLayout;
 import java.awt.FlowLayout;
-import java.util;
-import javax.swing.AbstractButton;
 public class Question_Panel extends JPanel{
 			private JLabel Question;
 			private ButtonGroup CBG = new ButtonGroup();
-			private GroupLayout GrpLyt = new GroupLayout(this);
 			private JRadioButton[] Answers = {new JRadioButton("Safe",false), new JRadioButton("Unsafe",false), new JRadioButton("Unobserved",false)};
 			protected Question_Panel(String Question){
 				this.Question = new JLabel(Question);
+				this.Question.setHorizontalAlignment((int)this.Question.LEFT_ALIGNMENT);
 				for(JRadioButton button : Answers){
 					CBG.add(button);
 				}
@@ -27,13 +25,5 @@ public class Question_Panel extends JPanel{
 			}
 			protected String getAnswer(){
 				return ((JRadioButton) CBG.getSelection()).getText();
-			}
-			protected void addGap(){
-				int sizeOfGap = Question.getFontMetrics(Question.getFont()).stringWidth(Question.getText());
-				for(JRadioButton button : Answers){
-					sizeOfGap += button.getFontMetrics(button.getFont()).stringWidth(button.getText());
-				}
-				sizeOfGap = (this.getWidth()-sizeOfGap)/6;
-				GrpLyt.setHorizontalGroup(GrpLyt.createSequentialGroup().addComponent(Question).addGap(sizeOfGap).addGroup(GrpLyt.createParallelGroup().addComponent(Answers[0]).addComponent(Answers[1]).addComponent(Answers[2])));
 			}
 		}
