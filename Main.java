af69e53705c6171f89c905795abf23ec9cb42ab6
Samuel C. Gurtler <StudentcareerSamgurtler@gmail.com>
2017-04-05 20:07:41 -0700

    Not Compiled

--- Main.java
+++ Main.java
@@ -1,38 +1,65 @@
 package EndUser;
 import javax.swing.JFrame;
-import java.awt.FlowLayout;
+//import java.awt.FlowLayout;
 import java.util.ArrayList;
-import javax.swing.JPanel;
-import java.awt.GridLayout;
+//import javax.swing.JPanel;
+//import java.awt.GridLayout;
 import java.util.Scanner;
 import java.io.File;
 import javax.swing.JLabel;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
+import javax.swing.GroupLayout;
+import javax.swing.ButtonGroup;
+import javax.swing.JRadioButton;
+import javax.swing.GroupLayout;
 	public class Main{
 		public static void main(String[] args)throws java.io.FileNotFoundException{
 			ArrayList<JFrame> JFrame = new ArrayList<JFrame>();
 			ArrayList<ArrayList<Question_Panel>> Arrlst_Questions = new ArrayList<ArrayList<Question_Panel>>();
+			/*ArrayList<ArrayList<ButtonGroup>> Arrlst_ButtonGroup = new ArrayList<ArrayList<ButtonGroup>>();
+			ArrayList<ArrayList<JRadioButton[]>> Arrlst_Button = new ArrayList<ArrayList<JRadioButton[]>>();
+			JRadioButton[] Buttons = {new JRadioButton("Safe",false), new JRadioButton("Unsafe",false), new JRadioButton("Unobserved",false)};
+			ArrayList<ArrayList<JLabel>> Arrlst_JLabel = new ArrayList<ArrayList<JLabel>>();*/
+			ArrayList<GroupLayout> Arrlst_GrpLyt = new ArrayList<GroupLayout>();
 			ArrayList<JLabel> Arrlst_Sections = new ArrayList<JLabel>();
 			Scanner scan = new Scanner(new File("./Questions.txt"));
 			Pattern p = Pattern.compile("[^:]+:");
 			String line = "";
-			for(int counter = -1;scan.hasNext();counter+=0){
+			for(int counter = -1,count = 0;scan.hasNext();counter+=0){
 				line = scan.nextLine();
 				Matcher m = p.matcher(line);
 				if(m.matches()){
 					counter++;
+					//count = 0;
 					System.out.println("Making new JLabel titled:"+line);
 					Arrlst_Sections.add(new JLabel(line));
 					Arrlst_Questions.add(new ArrayList<Question_Panel>());
 					System.out.println("Label ->"+line);
 				}
-				else Arrlst_Questions.get(counter).add(new Question_Panel(line)); 
+				else {
+					Arrlst_Questions.get(counter).add(new Question_Panel(line));
+					/*Arrlst_JLabel.get(counter).add(new JLabel(line));
+					Arrlst_Button.get(counter).add(Buttons);
+					ButtonGroup tmp = new ButtonGroup();
+					for(JRadioButton button : Arrlst_Button.get(counter).get(count)){
+						tmp.add(button);
+					}
+					Arrlst_ButtonGroup.get(counter).add(tmp);
+					count++;*/
+				}
 			}
 			for(int count = 0;Arrlst_Sections.size()>count;count++){
 				Arrlst_Sections.get(count).setHorizontalAlignment((int)Arrlst_Sections.get(count).CENTER_ALIGNMENT);
 				JFrame.add(new JFrame());
+				Arrlst_GrpLyt.add(new GroupLayout(JFrame.get(count)));
 				System.out.println("What JLabel may actually say:"+Arrlst_Sections.get(count).getText());
+				GroupLayout.SequentialGroup Order = Arrlst_GrpLyt.get(count).createSequentialGroup();
+				Order.addComponent(Arrlst_Sections.get(count));
+				for(int count1 = 0;Arrlst_Questions.get(count).size()>count1;count1++){
+					Order.addComponent(Arrlst_Questions.get(count).get(count1));
+				}
+				Arrlst_GrpLyt.get(count).setVerticalGroup(Order);
 				JFrame.get(count).add(Arrlst_Sections.get(count));
 				for(int count1 = 0;Arrlst_Questions.get(count).size()>count1;count1++){
 					JFrame.get(count).add(Arrlst_Questions.get(count).get(count1));
@@ -40,7 +67,7 @@ import java.util.regex.Matcher;
 				}
 				JFrame.get(count).setDefaultLookAndFeelDecorated(true);
 				JFrame.get(count).setDefaultCloseOperation(JFrame.get(count).DISPOSE_ON_CLOSE);
-				JFrame.get(count).setLayout(new GridLayout(0,1));
+				JFrame.get(count).setLayout(Arrlst_GrpLyt.get(count));
 				JFrame.get(count).pack();
 				JFrame.get(count).setVisible(true);
 			}
--- Question_Panel.java
+++ Question_Panel.java
@@ -10,6 +10,7 @@ import java.awt.FlowLayout;
 public class Question_Panel extends JPanel{
 			private JLabel Question;
 			private ButtonGroup CBG = new ButtonGroup();
+			private GroupLayout GrpLyt = new GroupLayout(this);
 			private JRadioButton[] Answers = {new JRadioButton("Safe",false), new JRadioButton("Unsafe",false), new JRadioButton("Unobserved",false)};
 			protected Question_Panel(String Question){
 				this.Question = new JLabel(Question);
@@ -26,4 +27,14 @@ public class Question_Panel extends JPanel{
 			protected String getAnswer(){
 				return ((JRadioButton) CBG.getSelection()).getText();
 			}
-		}
+			protected void addGap(){
+				int sizeOfGap = Question.getFontMetrics(Question.getFont()).stringWidth(Question.getText());
+				for(JRadioButton button : Answers){
+					sizeOfGap += button.getFontMetrics(button.getFont()).stringWidth(button.getText());
+				}
+				System.out.println("Width:"+this.getWidth());
+				sizeOfGap = (this.getWidth()-sizeOfGap)/6;
+				GrpLyt.setHorizontalGroup(GrpLyt.createSequentialGroup().addComponent(Question).addGap(sizeOfGap).addGroup(GrpLyt.createParallelGroup().addComponent(Answers[0]).addComponent(Answers[1]).addComponent(Answers[2])));
+			}
+}
+
