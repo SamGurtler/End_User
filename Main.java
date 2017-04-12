d511bafa52d2c90cfff68ba3401e8c43da8ce60f
Samuel C. Gurtler <StudentcareerSamgurtler@gmail.com>
2017-04-12 11:38:02 -0700

    Runs but not functional.
    Commented out buttons until null pointer location is found.

--- Main.java
+++ Main.java
@@ -14,11 +14,11 @@ import javax.swing.JRadioButton;
 import javax.swing.GroupLayout;
 	public class Main{
 		public static void main(String[] args)throws java.io.FileNotFoundException{
-			JFrame JFrame = new JFrame();
+			JFrame mainFrame = new JFrame();
 			CardLayout menuPages = new CardLayout();
 			CardLayout pages = new CardLayout();
 			JPanel[] Menu = new JPanel[3];
-			ArrayList<JPanel> JPanel = new ArrayList<JPanel>();
+			ArrayList<JPanel> cards = new ArrayList<JPanel>();
 			ArrayList<ArrayList<ButtonGroup>> Arrlst_ButtonGroup = new ArrayList<ArrayList<ButtonGroup>>();
 			ArrayList<ArrayList<JRadioButton[]>> Arrlst_Button = new ArrayList<ArrayList<JRadioButton[]>>();
 			JRadioButton[] Buttons = {new JRadioButton("Safe",false), new JRadioButton("Unsafe",false), new JRadioButton("Unobserved",false)};
@@ -33,12 +33,14 @@ import javax.swing.GroupLayout;
 				if(m.matches()){
 					counter++;
 					count = 0;
-					System.out.println("Making new JLabel titled:"+line);
+					//Test Line
+					//System.out.println("Making new JLabel titled:"+line);
 					Arrlst_Sections.add(new JLabel(line));
 					Arrlst_ButtonGroup.add(new ArrayList<ButtonGroup>());
 					Arrlst_Button.add(new ArrayList<JRadioButton[]>());
 					Arrlst_JLabel.add(new ArrayList<JLabel>());
-					System.out.println("Label ->"+line);
+					//Test line
+					//System.out.println("Label ->"+line);
 				}
 				else {
 					Arrlst_JLabel.get(counter).add(new JLabel(line));
@@ -54,27 +56,30 @@ import javax.swing.GroupLayout;
 			ArrayList<GroupLayout> Arrlst_GrpLyt = new ArrayList<GroupLayout>();
 			for(int count = 0;Arrlst_Sections.size()>count;count++){
 				Arrlst_Sections.get(count).setHorizontalAlignment((int)Arrlst_Sections.get(count).CENTER_ALIGNMENT);
-				JPanel.add(new JPanel());
-				Arrlst_GrpLyt.add(new GroupLayout(JPanel.get(count)));
+				cards.add(new JPanel());
+				Arrlst_GrpLyt.add(new GroupLayout(cards.get(count)));
 				Arrlst_GrpLyt.get(count).setAutoCreateContainerGaps(true); 
-				System.out.println("What JLabel may actually say:"+Arrlst_Sections.get(count).getText());
+				//Test Line
+				//System.out.println("What JLabel may actually say:"+Arrlst_Sections.get(count).getText());
 				GroupLayout.ParallelGroup verticalGroup = Arrlst_GrpLyt.get(count).createParallelGroup();
 				GroupLayout.SequentialGroup verticalJLabel = Arrlst_GrpLyt.get(count).createSequentialGroup();
 				for(int count1 = 0;Arrlst_JLabel.get(count).size()>count1;count1++){
 					verticalJLabel.addComponent(Arrlst_JLabel.get(count).get(count1));
 				}
 				GroupLayout.SequentialGroup verticalJRadioButton = Arrlst_GrpLyt.get(count).createSequentialGroup();
-				for(int count1 = 0;Arrlst_Button.get(count).size()>count1;count1++){
+				//Comment back in when solution for null pointer exception is found.
+				/*for(int count1 = 0;Arrlst_Button.get(count).size()>count1;count1++){
 					GroupLayout.ParallelGroup tmpVerticalButtonGroup = Arrlst_GrpLyt.get(count).createParallelGroup(GroupLayout.Alignment.TRAILING);
 					for(JRadioButton button : Arrlst_Button.get(count).get(count1)){
+						System.out.println("Count:"+count+" Count1:"+count1);
+						System.out.println(button.getBounds().toString());
 						tmpVerticalButtonGroup.addComponent(button);
 					}
 					verticalJRadioButton.addGroup(tmpVerticalButtonGroup);
-				}
+				}*/
 				verticalGroup.addGroup(verticalJLabel);
 				verticalGroup.addGroup(verticalJRadioButton);
 				Arrlst_GrpLyt.get(count).setVerticalGroup(Arrlst_GrpLyt.get(count).createSequentialGroup().addComponent(Arrlst_Sections.get(count)).addGroup(verticalGroup));
-				
 				GroupLayout.SequentialGroup horizontalGroup = Arrlst_GrpLyt.get(count).createSequentialGroup();
 				horizontalGroup.addGroup(Arrlst_GrpLyt.get(count).createParallelGroup(GroupLayout.Alignment.CENTER)).addComponent(Arrlst_Sections.get(count));
 				GroupLayout.ParallelGroup horizontalJLabel = Arrlst_GrpLyt.get(count).createParallelGroup();
@@ -82,34 +87,36 @@ import javax.swing.GroupLayout;
 					horizontalJLabel.addComponent(Arrlst_JLabel.get(count).get(count1));
 				}
 				horizontalGroup.addGroup(horizontalJLabel);
-				for(int count1 = 0;3>count1;count1++){
+				//Comment back in when solution for null pointer exception is found.
+				/*for(int count1 = 0;3>count1;count1++){
 					GroupLayout.ParallelGroup tmpHorizontalButtonGroup = Arrlst_GrpLyt.get(count).createParallelGroup();
 					for(int count2 = 0;Arrlst_Button.get(count).size()>count2;count2++){
 						tmpHorizontalButtonGroup.addComponent(Arrlst_Button.get(count).get(count2)[count1]);
 					}
 					horizontalGroup.addGroup(tmpHorizontalButtonGroup);
-				}
+				}*/
 				Arrlst_GrpLyt.get(count).setHorizontalGroup(horizontalGroup);
-				JPanel.get(count).setLayout(Arrlst_GrpLyt.get(count));
-				JPanel.get(count).add(Arrlst_Sections.get(count));
+				cards.get(count).setLayout(Arrlst_GrpLyt.get(count));
+				cards.get(count).add(Arrlst_Sections.get(count));
 				for(int count1 = 0;Arrlst_JLabel.get(count).size()>count1;count1++){
-					JPanel.get(count).add(Arrlst_JLabel.get(count).get(count1));
-					for(JRadioButton button : Arrlst_Button.get(count).get(count1)){
-						JPanel.get(count).add(button);
-					}
+					cards.get(count).add(Arrlst_JLabel.get(count).get(count1));
+					//Comment back in when solution for null pointer exception is found.
+					/*for(JRadioButton button : Arrlst_Button.get(count).get(count1)){
+						cards.get(count).add(button);
+					}*/
 				}
-				JPanel.get(count).validate();
-				pages.addLayoutComponent(JPanel.get(count),/*Arrlst_GrpLyt.get(count).toString()*/null);
+				//Not sure what to put for constraints
+				pages.addLayoutComponent(cards.get(count),Arrlst_GrpLyt.get(count).toString());
 			}
 			Menu[1] = new JPanel(pages);
-			for(int count = 0;JPanel.size()>count;count++){
-				Menu[1].add(JPanel.get(count));
+			for(int count = 0;cards.size()>count;count++){
+				Menu[1].add(cards.get(count));
 			}
-			JFrame.add(Menu[1]);
-			JFrame.setDefaultLookAndFeelDecorated(true);
-			JFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-			JFrame.pack();
-			JFrame.setVisible(true);
+			mainFrame.add(Menu[1]);
+			mainFrame.setDefaultLookAndFeelDecorated(true);
+			mainFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+			mainFrame.pack();
+			mainFrame.setVisible(true);
 			pages.first(Menu[1]);
 		}
 	}
